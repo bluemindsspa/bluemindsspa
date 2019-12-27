a98581f816c2618f56785560f52524a77439845e
RetargetingCL <54725593+RetargetingCL@users.noreply.github.com>
2019-12-27 13:08:02 -0300

    Update main.py

--- payment_flow/controllers/main.py
+++ payment_flow/controllers/main.py
@@ -43,14 +43,13 @@ class FlowController(http.Controller):
     ], type='http', auth='public', csrf=False, website=True)
     def flow_form_feedback(self, payment_tx=None, **post):
         _logger.warning("post %s, pay %s" %(post, payment_tx))
-        payment_tx.sudo().form_feedback(tx_data, 'flow')
         if not payment_tx:
             return
         tx_data = payment_tx.acquirer_id.flow_getTransaction(post)
         if not post['token']:
             message = {
                 'header': 'Oops!. La transacción no se ha podido terminar.',
-                'body': 'Orden de compra ' + payment_tx.reference,
+                
                 'detail':
                     '<p>Los posibles causas pueden ser:</p>'+
                     '<ul><li>Error en el ingreso de los datos de su tarjeta de Crédito o Débito (fecha y/o código de seguridad).</li>'+
@@ -59,7 +58,7 @@ class FlowController(http.Controller):
             }
         else:
             tx_data._token = post['token']
-            
+            payment_tx.sudo().form_feedback(tx_data, 'flow')
             #coloque estas líneas para colocar el status del pedido A facturar y a Pedido de Venta
             payment= payment_tx.reference[0:7]
             sale = request.env['sale.order'].search([('name', '=', payment)])
