c9aac98e095810a4ed24d324b5511168c4195ac2
RetargetingCL <54725593+RetargetingCL@users.noreply.github.com>
2019-12-30 12:19:32 -0300

    Update main.py

--- payment_flow/controllers/main.py
+++ payment_flow/controllers/main.py
@@ -43,6 +43,8 @@ class FlowController(http.Controller):
     ], type='http', auth='public', csrf=False, website=True)
     def flow_form_feedback(self, payment_tx=None, **post):
         _logger.warning("post %s, pay %s" %(post, payment_tx))
+        logging.info(post)
+        logging.info('POOOOOOOOOOOOOOOOST')
         if not payment_tx:
             return
         tx_data = payment_tx.acquirer_id.flow_getTransaction(post)
